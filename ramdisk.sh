7abc97972ea77be0eed4068f4e0d50de6dc6fc26
Jimin Hsieh <jiminbass@gmail.com>
2014-01-06 19:39:41 +0800

    add interactiv shell

--- ramdisk.sh
+++ ramdisk.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Plz execute script with how much size you want to allocate
+echo -n "How much ramdisk size you want assign[MB]? "
+read size 
 
 sudo sed -i '$i# ramdisk' /etc/rc.local
 sudo sed -i '$imkdir -p /media/ramdisk' /etc/rc.local
-sudo sed -i '$imount -t tmpfs -o size=$1M,nr_inodes=0 tmpfs /media/ramdisk' /etc/rc.local
+sudo sed -i '$imount -t tmpfs -o size='"$size"'M,nr_inodes=0 tmpfs /media/ramdisk' /etc/rc.local
 sudo sed -i '$i\\' /etc/rc.local
