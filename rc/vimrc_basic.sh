b67254c17112a58ed05f7f5fb20bf467f9139172
Jimin Hsieh <jimin.hsieh.engineer@gmail.com>
2018-03-26 21:34:25 +0800

    Config vimrc.

--- rc/vimrc_basic.sh
+++ rc/vimrc_basic.sh
@@ -7,4 +7,14 @@ set backspace=indent,eol,start
 syntax on
 colorscheme desert
 
+set number
+
+filetype plugin indent on
+" show existing tab with 4 spaces width
+set tabstop=2
+" when indenting with '>', use 4 spaces width
+set shiftwidth=2
+" On pressing tab, insert 4 spaces
+set expandtab
+
 EOF
